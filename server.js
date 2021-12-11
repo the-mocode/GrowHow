8366f6e25a368f417f2e382af47726001676f8b3
Mohamad <m.antar1996@hotmail.com>
2021-12-11 15:53:30 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3001; // Note: using a different port to normal
+
+app.use(express.static("./client/build"));
+
+app.use(express.json());
+
+
+app.get("/api/test", (req, res) => {
+  res.json({ result: "success" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
